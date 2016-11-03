b32dbed2a8aee3045ccf46c65c8cd379995db1c5
Thomas Kolar <thomas.kolar@email.de>
2016-11-04 00:52:38 +0100

    Adds a very basic form with some hardcoded ingredients that creates an influx line protocol write statement

--- index.ios.js
+++ index.ios.js
@@ -4,34 +4,52 @@ import {
     Text,
     TextInput,
     View,
-    PickerIOS
+    PickerIOS,
+    DatePickerIOS,
+    TouchableHighlight
 } from 'react-native';
 
 const PickerItemIOS = PickerIOS.Item;
 
 class AnnotationWriter extends Component {
+    static defaultProps = {
+        date: new Date(),
+        tag: 'manual'
+    };
+
     constructor(props) {
         super(props);
         this.state = {
             message: '',
-            tag: 'manual'
+            tag: this.props.tag,
+            date: this.props.date,
+            result: ''
         };
     }
+
+    onDateChange = (date) => {
+        this.setState({date: date});
+    };
+
+    onPressButton = () => {
+        this.setState({result: 'annotations,' + this.state.tag + '=' + this.state.tag + ' text=' + this.state.message + ' ' + this.state.date.getTime() * 1000000});
+    };
 
     render() {
         return (
             <View style={{padding: 10}}>
-                <Text style={{padding: 10, fontSize: 42}}>
+                <Text style={{padding: 10, fontSize: 20}}>
+                    {this.state.result}
+                </Text>
+
+                <Text style={{padding: 10, fontSize: 20}}>
                     Message
                 </Text>
                 <TextInput
-                    style={{height: 40}}
+                    style={{height: 20}}
                     placeholder="Type here to the message of the annotation!"
                     onChangeText={(message) => this.setState({message})}
                 />
-                <Text style={{padding: 10, fontSize: 42}}>
-                    {this.state.message}
-                </Text>
                 <PickerIOS
                     selectedValue={this.state.tag}
                     onValueChange={(tag) => this.setState({tag})}>
@@ -46,9 +64,18 @@ class AnnotationWriter extends Component {
                         label='live-deploy'
                     />
                 </PickerIOS>
-                <Text style={{padding: 10, fontSize: 42}}>
-                    {this.state.tag}
-                </Text>
+                <DatePickerIOS
+                    date={this.state.date}
+                    mode="datetime"
+                    timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+                    onDateChange={this.onDateChange}
+                />
+
+                <TouchableHighlight onPress={this.onPressButton}>
+                    <Text style={{padding: 10, fontSize: 20}}>
+                        Annotate!
+                    </Text>
+                </TouchableHighlight>
             </View>
         );
     }
