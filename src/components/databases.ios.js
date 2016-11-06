3839c08ae0f1d5bf40066d287d722772d52f2398
Thomas Kolar <thomas.kolar@email.de>
2016-11-06 17:36:50 +0100

    Adds a basic form that fires a request an influx database with the credentials & config provided by the form

--- src/components/databases.ios.js
+++ src/components/databases.ios.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
 import {
+    Text,
+    TextInput,
     View,
-    Text
+    TouchableHighlight
 } from 'react-native';
 
 class DatabasesView extends Component {
-    static defaultProps = {};
+    static defaultProps = {
+        id: undefined,
+        url: undefined,
+        alias: undefined,
+        port: 8086,
+        name: undefined,
+        measuremnt: undefined,
+        username: undefined,
+        password: undefined
+    };
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            id: this.props.id,
+            url: this.props.url,
+            alias: this.props.alias,
+            port: this.props.port,
+            name: this.props.name,
+            measurement: this.props.measurement,
+            username: this.props.username,
+            password: this.props.password
+        };
     }
+
+    onPressButton = () => {
+        fetch(
+            'http://' + this.state.url + ':' + this.state.port + '/query?db=' + this.state.name + '&q=SELECT%20*%20FROM%20' + this.state.measurement + '%20LIMIT%201',
+            {
+                method: 'GET'
+            }
+        );
+    };
 
     render() {
         return (
@@ -18,6 +47,46 @@ class DatabasesView extends Component {
                 <Text style={{padding: 10, fontSize: 20}}>
                     Databases
                 </Text>
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="Alias for the database"
+                    onChangeText={(alias) => this.setState({alias})}
+                />
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="url"
+                    onChangeText={(url) => this.setState({url})}
+                />
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="port"
+                    onChangeText={(port) => this.setState({port})}
+                />
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="Database name"
+                    onChangeText={(name) => this.setState({name})}
+                />
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="Measurement"
+                    onChangeText={(measurement) => this.setState({measurement})}
+                />
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="Username"
+                    onChangeText={(username) => this.setState({username})}
+                />
+                <TextInput
+                    style={{height: 20}}
+                    placeholder="Password"
+                    onChangeText={(password) => this.setState({password})}
+                />
+                <TouchableHighlight onPress={this.onPressButton}>
+                    <Text style={{padding: 10, fontSize: 20}}>
+                        (Test &) Save
+                    </Text>
+                </TouchableHighlight>
             </View>
         );
     }
