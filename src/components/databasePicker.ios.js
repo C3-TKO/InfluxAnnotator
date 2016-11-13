83301b6d3e4767c3758938ab6e75ec6645e32ef1
Thomas Kolar <thomas.kolar@email.de>
2016-11-13 15:46:20 +0100

    Adds proper handling of adding/removing databases

--- src/components/databasePicker.ios.js
+++ src/components/databasePicker.ios.js
@@ -18,6 +18,17 @@ class DatabasePickerIOS extends Component {
         };
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(typeof this.state.index == 'undefined' && nextProps.databases.length > 0) {
+            alert('Setting to zero');
+            this.setState({index: 0})
+        }
+        if(this.state.index > nextProps.databases.length - 1) {
+            alert('Setting to new last');
+            this.setState({index: nextProps.databases.length - 1})
+        }
+    }
+
     render() {
         let databasePicker = null;
         if ( this.props.databases.length > 0 ) {
