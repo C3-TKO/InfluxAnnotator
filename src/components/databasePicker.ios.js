e3a821681a33ede882a06e3c66926c074221479e
Thomas Kolar <thomas.kolar@email.de>
2016-11-14 12:29:11 +0100

    Removes no longer necessary managament of the currently selected database

--- src/components/databasePicker.ios.js
+++ src/components/databasePicker.ios.js
@@ -9,25 +9,6 @@ import {
 const PickerItemIOS = PickerIOS.Item;
 
 class DatabasePickerIOS extends Component {
-    static defaultProps = {
-        index: undefined
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            index: this.props.index
-        };
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if(typeof this.state.index == 'undefined' && nextProps.databases.credentials.length > 0) {
-            this.setState({index: 0})
-        }
-        if(this.state.index > nextProps.databases.credentials.length - 1) {
-            this.setState({index: nextProps.databases.credentials.length - 1})
-        }
-    }
 
     render() {
         let databasePicker = null;
