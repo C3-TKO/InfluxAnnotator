6716734d1c4b07c761af60af0c2b964ad781643f
Thomas Kolar <thomas.kolar@email.de>
2016-11-20 15:16:00 +0100

    Adds AsyncStorage as persistence layer

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist'
+import { AsyncStorage } from 'react-native'
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
@@ -8,7 +10,19 @@ import InfluxAnnotatorApp from './influxAnnotatorApp.ios';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
-const store = createStoreWithMiddleware(reducer);
+
+const store = createStore(
+    combineReducers(reducers),
+    {},
+    compose(
+        autoRehydrate(),
+        applyMiddleware(thunk)
+    )
+)
+
+persistStore(store, {storage: AsyncStorage}, () => {
+    alert('restored')
+})
 
 export default class App extends Component {
     render() {
