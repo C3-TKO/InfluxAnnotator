c3b268bd8f9dcb32ea034d1e346556a895e46de1
Thomas Kolar <thomas.kolar@email.de>
2016-11-21 21:33:11 +0100

    Adds the request for retrieving annotations

--- src/components/annotations.ios.js
+++ src/components/annotations.ios.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {
     View,
-    Text
+    Text,
+    TouchableHighlight
 } from 'react-native';
 
 class AnnotationsView extends Component {
@@ -12,9 +14,24 @@ class AnnotationsView extends Component {
         this.state = {};
     }
 
+    loadAnnotations = () => {
+        const database = this.props.databases.credentials[this.props.databases.selected];
+        fetch(
+            'http://' + database.url + ':' + database.port + '/query?db=' + database.name + '&q=SELECT * FROM ' + database.measurement,
+            {
+                method: 'GET'
+            }
+        );
+    };
+
     render() {
         return (
             <View style={{padding: 10}}>
+                <TouchableHighlight onPress={this.loadAnnotations}>
+                    <Text style={{padding: 10, fontSize: 20}}>
+                        Read
+                    </Text>
+                </TouchableHighlight>
                 <Text style={{padding: 10, fontSize: 20}}>
                     Annotations
                 </Text>
@@ -23,4 +40,8 @@ class AnnotationsView extends Component {
     }
 }
 
-module.exports = AnnotationsView;
+export default connect(
+    state => ({
+        databases: state.databases
+    })
+)(AnnotationsView);
