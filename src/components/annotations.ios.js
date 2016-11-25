4df4a8be0caea25a818e0407928cccd32e703110
Thomas Kolar <thomas.kolar@email.de>
2016-11-25 23:21:56 +0100

    Extracts annotations from result

--- src/components/annotations.ios.js
+++ src/components/annotations.ios.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import {
     ScrollView,
     Text,
+    ListView,
     TouchableHighlight
 } from 'react-native';
 
@@ -24,12 +25,37 @@ class AnnotationsView extends Component {
         )
         .then((response) => response.json())
         .then((responseJson) => {
-            console.log(responseJson);
-            return responseJson;
+            console.log(responseJson.results[0].series[0].values);
+            return responseJson.results[0].series[0].values;
         })
         .catch((error) => {
             console.error(error)
         });
+    };
+
+    renderAnnotations = () => {
+        const annotations = this.loadAnnotations();
+
+        console.log(annotations);
+
+        /*
+        { results:
+            [ { series:
+                [ { name: 'annotations',
+                    columns: [ 'time', 'message', 'type' ],
+                    values:
+                        [ [ '2016-11-24T20:11:00Z', 'Test', 'manual' ],
+                            [ '2016-11-24T20:12:00Z', 'Booyakasha', 'manual' ],
+                            [ '2016-11-24T20:12:00.154Z', 'Test', 'manual' ] ] } ] } ] }
+*/
+        
+        return (
+            <ListView>
+                dataSource={annotations}
+                renderRow={(rowData) => <Text>{rowData[0] + rowData[1] + rowData[2]}</Text>}
+            </ListView>
+        )
+
     };
 
     render() {
@@ -43,6 +69,7 @@ class AnnotationsView extends Component {
                 <Text style={{padding: 10, fontSize: 20}}>
                     Annotations
                 </Text>
+                {this.renderAnnotations()}
             </ScrollView>
         );
     }
