9264214501430702d471c99fcac150cafa74a908
Thomas Kolar <thomas.kolar@email.de>
2017-01-19 23:05:09 +0100

    Migrates the input toggle from panza-dev

---
+++ src/components/panza-migrations/InputToggle.js
@@ -0,0 +1,101 @@
+import React, { PropTypes } from 'react'
+import {
+    Switch,
+    Platform,
+    StyleSheet
+} from 'react-native'
+import {
+    Base,
+    Text,
+    InputRowCell
+} from 'panza'
+
+import config from './config'
+
+/**
+ * An basic input row with a label and Switch, allowing
+ * the user to toggle a boolean value.
+ */
+
+class InputToggle extends React.Component {
+
+    static displayName = 'InputToggle'
+
+    static propTypes = {
+        style: PropTypes.any,
+        value: PropTypes.bool.isRequired,
+        backgroundColor: PropTypes.string,
+        label: PropTypes.string.isRequired,
+        onValueChange: PropTypes.func.isRequired,
+        editable: PropTypes.bool,
+        switchProps: PropTypes.object,
+        onTintColor: PropTypes.string
+    }
+
+    static defaultProps = {
+        backgroundColor: 'transparent',
+        editable: true,
+        onTintColor: 'success'
+    }
+
+    static contextTypes = {
+        panza: PropTypes.object
+    }
+
+    render () {
+        const {
+            label,
+            value,
+            editable,
+            onValueChange,
+            switchProps,
+            onTintColor
+            } = this.props
+
+        const {
+            panza
+            } = this.context
+
+        const { colors } = { ...config, ...panza || {} }
+        let tintColor = colors[onTintColor]
+
+        return (
+            <InputRowCell
+                style={{backgroundColor: '#ffffff'}}
+            >
+                <Base pl={2}>
+                    <Text
+                        style={{fontWeight: '600', fontSize: 15, backgroundColor: '#ffffff'}}
+                    >
+                        {label}
+                    </Text>
+                </Base>
+                <Base flex={1}>
+                    <Switch
+                        style={styles.switch}
+                        value={value}
+                        edisabled={!editable}
+                        onTintColor={tintColor}
+                        {...switchProps}
+                        onValueChange={onValueChange}
+                    />
+                </Base>
+            </InputRowCell>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    switch: {
+        alignSelf: 'flex-end',
+        marginRight: 16,
+        ...Platform.select({
+            android: {
+                marginVertical: 13
+            }
+        })
+    }
+})
+
+module.exports = InputToggle
