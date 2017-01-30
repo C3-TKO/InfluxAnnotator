17a00f076e40b3d229409acc8267c83e14ebc6e7
Thomas Kolar <thomas.kolar@email.de>
2017-01-30 22:16:49 +0100

    Migrates delete action

--- src/reducers/databases.js
+++ src/reducers/databases.js
@@ -23,33 +23,30 @@ export default function databases(state = initialState, action = {}) {
             console.log(nextState);
             return nextState;
         case types.EDIT_DATABASE:
-            console.log(nextState);
-            console.log(action);
             nextState = nextState.set('selected', action.index); // Needs to be set for the case of overwriting an existing database
             nextState = nextState.setIn(['credentials', action.index], action.database)
             console.log(nextState);
             return nextState;
-        /*
         case types.DELETE_DATABASE:
             // Checking for state of the selected index will be out of boundary
-            let selected = state.selected;
+            let selected = nextState.get('selected');
 
             // If the very last database of the list is to deleted, the index needs to set to the second last database
-            if ((state.credentials.length - 1) === action.index) {
-                selected = state.credentials.length - 2;
+            if ((nextState.get('credentials').size - 1) === action.index) {
+                selected = nextState.get('credentials').size - 2;
             }
             // If the last database will be deleted the selected index needs to be set to undefined
-            if (state.credentials.length === 1) {
+            if (nextState.get('credentials').size === 1) {
                 selected = undefined;
             }
 
+            nextState = nextState.set('selected', selected);
+            nextState = nextState.deleteIn(['credentials', action.index]);
 
-            return {
-                ...state,
-                selected: selected,
-                credentials: state.credentials.filter((credential, index) => index != action.index)
-            };
+            console.log(nextState);
+            return nextState;
 
+        /*
         case types.SELECT_DATABASE:
             return {
                 ...state,
